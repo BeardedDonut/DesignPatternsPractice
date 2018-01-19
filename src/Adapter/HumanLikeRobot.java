7cac1c8ba09b2057de992b9874806a1241ffca6d
navid <navid9675@gmail.com>
2018-01-19 07:14:38 +0330

    + Adapter

---
+++ src/Adapter/HumanLikeRobot.java
@@ -0,0 +1,41 @@
+package Adapter;
+
+/**
+ * Created by navid on 1/19/18.
+ */
+public class HumanLikeRobot extends Human {
+    private Robot core;
+
+    public HumanLikeRobot(String nickname,Robot core) {
+        super(nickname);
+        this.core = core;
+    }
+
+    @Override
+    public void walk() {
+        System.out.println("Walk:");
+        core.move();
+        System.out.println();
+    }
+
+    @Override
+    public void saySomething() {
+        System.out.println("Say Something:");
+        core.printSomething();
+        System.out.println();
+    }
+
+    @Override
+    public void passCaptchaTest() {
+        System.out.println("Pass Captcha:");
+        core.passCaptcha();
+        System.out.println();
+    }
+
+    @Override
+    public void whoAreYou() {
+        System.out.println("My nickname is:" + this.name);
+        core.printId();
+        System.out.println();
+    }
+}
